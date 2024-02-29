917ceed32d3a243fd7934eb9d6e72b113ea8c2fc
remco van der Velde <remco.pc@outlook.com>
2024-02-29 20:37:55 +0100

    inotify

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -99,12 +99,12 @@ trait Main {
         echo $options['dir'] . PHP_EOL;
         $write = [];
         $write[] = '#!/bin/bash';
-        $write[] = 'inotifywait -m ' . $options['dir']  .' -e create -e moved_to | ';
-        $write[] = 'while read -r directory action file; do';
-        $write[] = 'if [[ "$file" =~ .*json$ ]]; then # Does the file end with .xml?';
-        $write[] = 'echo "xml file" # If so, do your thing here!';
-        $write[] = 'fi';
-        $write[] = 'done';
+        $write[] = 'inotifywait -m ' . $options['dir']  .' -e create -e moved_to | \\';
+        $write[] = 'while read -r directory action file; do \\';
+        $write[] = 'if [[ "$file" =~ .*json$ ]]; then # Does the file end with .xml? \\';
+        $write[] = 'echo "xml file" # If so, do your thing here! \\';
+        $write[] = 'fi \\';
+        $write[] = 'done \\';
         $write[] = '';
 
         Dir::create($dir, Dir::CHMOD);
